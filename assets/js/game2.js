6448b02ccc47b2b0dda6d3bfdb583e4ff58f4727
ThomasKyle <thomas19992010@hotmail.com>
2021-01-08 21:01:41 -0500

    added start, next, highscore btn and mock questions

---
+++ assets/js/game2.js
@@ -0,0 +1,4 @@
+const StartButton = document.getElementById('start-btn');
+const nextButton = docuemnt.getElementById('next-btn');
+const highscoreButton = document.getElementById('highscore-btn');
+const questionContainerEl = document.getElementById('question-container');
