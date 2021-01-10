5c527e19af6493b4acf22f394b2ac95030d02d80
ThomasKyle <thomas19992010@hotmail.com>
2021-01-10 18:46:35 -0500

    Restarting

--- assets/js/game2.js
+++
@@ -1,4 +0,0 @@
-const StartButton = document.getElementById('start-btn');
-const nextButton = docuemnt.getElementById('next-btn');
-const highscoreButton = document.getElementById('highscore-btn');
-const questionContainerEl = document.getElementById('question-container');
